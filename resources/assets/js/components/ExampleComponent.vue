071eba0840b3314aba8e6c14121e009671f542a2
Michael Thorne <michael.thorne@gmail.com>
2018-06-07 17:10:56 +0200

    Added ExampleComponent to view

--- resources/assets/js/components/ExampleComponent.vue
+++ resources/assets/js/components/ExampleComponent.vue
@@ -6,7 +6,7 @@
                     <div class="card-header">Example Component</div>
 
                     <div class="card-body">
-                        I'm an example component.
+                        I’m an example Vue.js component.
                     </div>
                 </div>
             </div>
@@ -21,3 +21,7 @@
         }
     }
 </script>
+
+<style scoped>
+
+</style>
